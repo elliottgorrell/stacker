c79b7cddd052f5ae015a72ca31a639fdaa835ad3
Elliott Gorrell <elliottgorrell1@gmail.com>
2017-09-04 16:01:27 +1000

    Added test for bug fix

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,15 +1,34 @@
 from unittest import TestCase
 
 import pytest
-
+import os
+from mock import MagicMock
 from stacker import deploy
 
 
 class DeployExecutorTest(TestCase):
+
+    cf_json = os.path.join(os.path.dirname(__file__),'resources/cloudformation.json')
 
     def test_error_thrown_on_incorrect_config_file_format(self):
         with pytest.raises(deploy.DeployException):
             executor = deploy.DeployExecutor()
             executor.load_parameters("config.txt")
 
-    def test_
+    def test_load_json_cloudformation(self):
+        executor = deploy.DeployExecutor()
+        executor.load_cloudformation(self.cf_json)
+
+    def test_deploy_with_no_config_file(self):
+        executor = deploy.DeployExecutor()
+
+        executor.cf_client = MagicMock()
+
+        executor.cf_client.create_change_set = MagicMock()
+        executor.cf_client.wait_for_change_set_to_complete = MagicMock()
+
+        executor.execute(stack_name="test-stack",template_name=self.cf_json,dry_run=True)
+
+        executor.cf_client.create_change_set.assert_called()
+        executor.cf_client.wait_for_change_set_to_complete.assert_called()
+
