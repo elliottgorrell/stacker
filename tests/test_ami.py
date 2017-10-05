6fd45dadba757e65784456da6a3747aab723d913
Elliott Gorrell <elliottgorrell1@gmail.com>
2017-10-06 10:48:48 +1100

    We were using TestCase’s and unittest style asserts yet we are using pytest. I have removed all this and change assertion style, let me know if this is wrong

--- tests/test_ami.py
+++ tests/test_ami.py
@@ -1,110 +1,97 @@
-from unittest import TestCase
-
 import pytest
 from mock import call, MagicMock
 
 from stacker import ami
 from stacker.cf_helper import utils as cf_utils
 
-class AMIExecutorTest(TestCase):
+def test_no_parameters():
 
-    def test_no_parameters(self):
+    executor = ami.AMIExecutor(None)
 
-        executor = ami.AMIExecutor(None)
+    with pytest.raises(cf_utils.DeployException) as ex:
+        executor.execute()
 
-        with pytest.raises(cf_utils.DeployException) as ex:
-            executor.execute()
+    assert "--artifact-id or --ami-id must be supplied for the search" == ex.value.message
 
-        self.assertEqual("--artifact-id or --ami-id must be supplied for the search", ex.value.message)
+def test_by_artifact_id_no_result():
 
-    def test_by_artifact_id_no_result(self):
+    executor = ami.AMIExecutor(None)
+    executor.ec2_client = MagicMock()
 
-        executor = ami.AMIExecutor(None)
-        executor.ec2_client = MagicMock()
+    mock_response = {"Images": []}
+    executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
 
-        mock_response = {"Images": []}
-        executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
+    with pytest.raises(cf_utils.DeployException) as ex:
+        result = executor.execute(artifact_id="artifact")
 
-        with pytest.raises(cf_utils.DeployException) as ex:
-            result = executor.execute(artifact_id="artifact")
+    assert [call.describe_images(Filters=[{'Values': ['artifact'], 'Name': 'tag:ArtifactID'}])] == executor.ec2_client.mock_calls
 
-        self.assertEqual([call.describe_images(Filters=[{'Values': ['artifact'], 'Name': 'tag:ArtifactID'}])],
-                         executor.ec2_client.mock_calls)
+    assert "No images found for search 'artifact'" == ex.value.message
 
-        self.assertEqual("No images found for search 'artifact'", ex.value.message)
+def test_by_artifact_id_with_single_result():
 
-    def test_by_artifact_id_with_single_result(self):
+    executor = ami.AMIExecutor(None)
+    executor.ec2_client = MagicMock()
 
-        executor = ami.AMIExecutor(None)
-        executor.ec2_client = MagicMock()
+    mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"}]}
+    executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
 
-        mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"}]}
-        executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
+    executor.execute(artifact_id="artifact")
 
-        executor.execute(artifact_id="artifact")
+    assert [call.describe_images(Filters=[{'Values': ['artifact'], 'Name': 'tag:ArtifactID'}])] == executor.ec2_client.mock_calls
 
-        self.assertEqual([call.describe_images(Filters=[{'Values': ['artifact'], 'Name': 'tag:ArtifactID'}])],
-                         executor.ec2_client.mock_calls)
+def test_by_artifact_id_with_duplicate_result():
 
-    def test_by_artifact_id_with_duplicate_result(self):
+    executor = ami.AMIExecutor(None)
+    executor.ec2_client = MagicMock()
 
-        executor = ami.AMIExecutor(None)
-        executor.ec2_client = MagicMock()
+    mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"},
+                                {"ImageId": "id-1235", "Name": "mock-image", "CreationDate": "today"}]}
 
-        mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"},
-                                    {"ImageId": "id-1235", "Name": "mock-image", "CreationDate": "today"}]}
+    executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
 
-        executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
+    with pytest.raises(cf_utils.DeployException) as ex:
+        result = executor.execute(artifact_id="artifact")
 
-        with pytest.raises(cf_utils.DeployException) as ex:
-            result = executor.execute(artifact_id="artifact")
+    assert [call.describe_images(Filters=[{'Values': ['artifact'], 'Name': 'tag:ArtifactID'}])] == executor.ec2_client.mock_calls
+    assert "More than 1 image found for search 'artifact'" == ex.value.message
 
-        self.assertEqual([call.describe_images(Filters=[{'Values': ['artifact'], 'Name': 'tag:ArtifactID'}])],
-                         executor.ec2_client.mock_calls)
+def test_by_ami_id_with_no_result():
 
-        self.assertEqual("More than 1 image found for search 'artifact'", ex.value.message)
+    executor = ami.AMIExecutor(None)
+    executor.ec2_client = MagicMock()
 
-    def test_by_ami_id_with_no_result(self):
+    mock_response = {"Images": []}
+    executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
 
-        executor = ami.AMIExecutor(None)
-        executor.ec2_client = MagicMock()
-
-        mock_response = {"Images": []}
-        executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
-
-        with pytest.raises(cf_utils.DeployException) as ex:
-            executor.execute(ami_id="ami-1234")
-
-        self.assertEqual([call.describe_images(ImageIds=['ami-1234'])],
-                         executor.ec2_client.mock_calls)
-
-        self.assertEqual("No images found for search 'ami-1234'", ex.value.message)
-
-    def test_by_ami_id_with_single_result(self):
-
-        executor = ami.AMIExecutor(None)
-        executor.ec2_client = MagicMock()
-
-        mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"}]}
-        executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
-
+    with pytest.raises(cf_utils.DeployException) as ex:
         executor.execute(ami_id="ami-1234")
 
-        self.assertEqual([call.describe_images(ImageIds=['ami-1234'])],
-                         executor.ec2_client.mock_calls)
+    assert [call.describe_images(ImageIds=['ami-1234'])] == executor.ec2_client.mock_calls
+    assert "No images found for search 'ami-1234'" == ex.value.message
 
-    def test_by_ami_id_with_multiple_results(self):
-        executor = ami.AMIExecutor(None)
-        executor.ec2_client = MagicMock()
+def test_by_ami_id_with_single_result():
 
-        mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"},
-                                    {"ImageId": "id-1235", "Name": "mock-image", "CreationDate": "today"}]}
-        executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
+    executor = ami.AMIExecutor(None)
+    executor.ec2_client = MagicMock()
 
-        with pytest.raises(cf_utils.DeployException) as ex:
-            executor.execute(ami_id="ami-1234")
+    mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"}]}
+    executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
 
-        self.assertEqual([call.describe_images(ImageIds=['ami-1234'])],
-                         executor.ec2_client.mock_calls)
+    executor.execute(ami_id="ami-1234")
 
-        self.assertEqual("More than 1 image found for search 'ami-1234'", ex.value.message)
+    assert [call.describe_images(ImageIds=['ami-1234'])] == executor.ec2_client.mock_calls
+
+def test_by_ami_id_with_multiple_results():
+    executor = ami.AMIExecutor(None)
+    executor.ec2_client = MagicMock()
+
+    mock_response = {"Images": [{"ImageId": "id-1234", "Name": "mock-image", "CreationDate": "today"},
+                                {"ImageId": "id-1235", "Name": "mock-image", "CreationDate": "today"}]}
+    executor.ec2_client.describe_images = MagicMock(return_value=mock_response)
+
+    with pytest.raises(cf_utils.DeployException) as ex:
+        executor.execute(ami_id="ami-1234")
+
+    assert [call.describe_images(ImageIds=['ami-1234'])] == executor.ec2_client.mock_calls
+    assert "More than 1 image found for search 'ami-1234'" == ex.value.message
--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-
 import pytest
 import os
 from mock import MagicMock
@@ -9,86 +7,83 @@ from moto import mock_cloudformation
 import boto3
 
 
-class DeployExecutorTest(TestCase):
+cf_json = os.path.join(os.path.dirname(__file__),'resources/cloudformation.json')
+cf_yaml_functions = os.path.join(os.path.dirname(__file__), 'resources/cf_functions.yaml')
+cf_json_functions = os.path.join(os.path.dirname(__file__), 'resources/cf_functions.json')
+config_json = os.path.join(os.path.dirname(__file__), 'resources/config.json')
 
-    cf_json = os.path.join(os.path.dirname(__file__),'resources/cloudformation.json')
-    cf_yaml_functions = os.path.join(os.path.dirname(__file__), 'resources/cf_functions.yaml')
-    cf_json_functions = os.path.join(os.path.dirname(__file__), 'resources/cf_functions.json')
-    config_json = os.path.join(os.path.dirname(__file__), 'resources/config.json')
-
-    def test_error_thrown_on_incorrect_config_file_format(self):
-        with pytest.raises(deploy.DeployException):
-            executor = deploy.DeployExecutor()
-            executor.load_parameters("config.txt")
-
-    def test_load_json_cloudformation(self):
+def test_error_thrown_on_incorrect_config_file_format():
+    with pytest.raises(deploy.DeployException):
         executor = deploy.DeployExecutor()
-        executor.load_cloudformation(self.cf_json)
+        executor.load_parameters("config.txt")
 
-    def test_deploy_with_no_config_file(self):
-        executor = deploy.DeployExecutor()
+def test_load_json_cloudformation():
+    executor = deploy.DeployExecutor()
+    executor.load_cloudformation(cf_json)
 
-        executor.cf_client = MagicMock()
+def test_deploy_with_no_config_file():
+    executor = deploy.DeployExecutor()
 
-        executor.cf_client.create_change_set = MagicMock()
-        executor.cf_client.wait_for_change_set_to_complete = MagicMock()
+    executor.cf_client = MagicMock()
 
-        executor.execute(stack_name="test-stack",template_name=self.cf_json)
+    executor.cf_client.create_change_set = MagicMock()
+    executor.cf_client.wait_for_change_set_to_complete = MagicMock()
 
-    def test_deploy_with_json_config(self):
-        executor = deploy.DeployExecutor()
+    executor.execute(stack_name="test-stack",template_name=cf_json)
 
-        executor.cf_client = MagicMock()
+def test_deploy_with_json_config():
+    executor = deploy.DeployExecutor()
 
-        executor.cf_client.create_change_set = MagicMock()
-        executor.cf_client.wait_for_change_set_to_complete = MagicMock()
+    executor.cf_client = MagicMock()
 
-        executor.execute(stack_name="test-stack",template_name=self.cf_json, config_filename=self.config_json, create=True)
+    executor.cf_client.create_change_set = MagicMock()
+    executor.cf_client.wait_for_change_set_to_complete = MagicMock()
 
-        executor.cf_client.create_change_set.assert_called()
-        executor.cf_client.wait_for_change_set_to_complete.assert_called()
+    executor.execute(stack_name="test-stack",template_name=cf_json, config_filename=config_json, create=True)
+
+    executor.cf_client.create_change_set.assert_called()
+    executor.cf_client.wait_for_change_set_to_complete.assert_called()
 
 
-    # Tests that we can parse yaml containing Cloudformation functions
-    def test_deploy_yaml_cf_with_functions(self):
-        executor = deploy.DeployExecutor()
+# Tests that we can parse yaml containing Cloudformation functions
+def test_deploy_yaml_cf_with_functions():
+    executor = deploy.DeployExecutor()
 
-        executor.cf_client = MagicMock()
+    executor.cf_client = MagicMock()
 
-        executor.cf_client.create_change_set = MagicMock()
-        executor.cf_client.wait_for_change_set_to_complete = MagicMock()
+    executor.cf_client.create_change_set = MagicMock()
+    executor.cf_client.wait_for_change_set_to_complete = MagicMock()
 
-        executor.execute(stack_name="test-stack", template_name=self.cf_yaml_functions, config_filename=self.config_json, create=True)
+    executor.execute(stack_name="test-stack", template_name=cf_yaml_functions, config_filename=config_json, create=True)
 
-        executor.cf_client.create_change_set.assert_called()
-        executor.cf_client.wait_for_change_set_to_complete.assert_called()
+    executor.cf_client.create_change_set.assert_called()
+    executor.cf_client.wait_for_change_set_to_complete.assert_called()
 
-    # Tests that we can parse yaml containing Cloudformation functions
-    def test_deploy_json_cf_with_functions(self):
-        executor = deploy.DeployExecutor()
+# Tests that we can parse json containing Cloudformation functions
+def test_deploy_json_cf_with_functions():
+    executor = deploy.DeployExecutor()
 
-        executor.cf_client = MagicMock()
+    executor.cf_client = MagicMock()
 
-        executor.cf_client.create_change_set = MagicMock()
-        executor.cf_client.wait_for_change_set_to_complete = MagicMock()
+    executor.cf_client.create_change_set = MagicMock()
+    executor.cf_client.wait_for_change_set_to_complete = MagicMock()
 
-        executor.execute(stack_name="test-stack", template_name=self.cf_json_functions,
-                         config_filename=self.config_json, create=True)
+    executor.execute(stack_name="test-stack", template_name=cf_json_functions, config_filename=config_json, create=True)
 
-        executor.cf_client.create_change_set.assert_called()
-        executor.cf_client.wait_for_change_set_to_complete.assert_called()
+    executor.cf_client.create_change_set.assert_called()
+    executor.cf_client.wait_for_change_set_to_complete.assert_called()
 
-    # Tests that we can parse json containing Cloudformation functions
-    def test_full_deploy_json(self):
-        executor = deploy.DeployExecutor()
+# Tests that we can parse json containing Cloudformation functions
+def test_full_deploy_json():
+    executor = deploy.DeployExecutor()
 
-        executor.cf_client.create_change_set = MagicMock()
-        executor.cf_client.wait_for_change_set_to_complete = MagicMock()
-        executor.cf_client.describe_change_set = MagicMock()
-        executor.cf_client.execute_change_set = MagicMock()
-        executor.cf_client.describe_stacks = MagicMock()
+    executor.cf_client.create_change_set = MagicMock()
+    executor.cf_client.wait_for_change_set_to_complete = MagicMock()
+    executor.cf_client.describe_change_set = MagicMock()
+    executor.cf_client.execute_change_set = MagicMock()
+    executor.cf_client.describe_stacks = MagicMock()
 
-        executor.execute(stack_name="test-stack",template_name=self.cf_json_functions, config_filename=self.config_json, create=True)
+    executor.execute(stack_name="test-stack",template_name=cf_json_functions, config_filename=config_json, create=True)
 
-        executor.cf_client.create_change_set.assert_called()
-        executor.cf_client.wait_for_change_set_to_complete.assert_called()
+    executor.cf_client.create_change_set.assert_called()
+    executor.cf_client.wait_for_change_set_to_complete.assert_called()
