3b25ae5e9b75436c5998a74f38569d360b1d496f
Elliott Gorrell <elliottgorrell1@gmail.com>
2017-10-19 09:41:01 +1100

    Yaml constructor options put inside cf_helper as it is loaded into stacker - need to put this  in a better place in the future doesn’t feel right sitting here

--- stacker/__init__.py
+++ stacker/__init__.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 from stacker import *
-import yaml
-
-
-def construct_function_ignore(loader, parameter):
-    return parameter
-
-
-yaml.add_constructor(u"!FindInMap", construct_function_ignore)
-yaml.add_constructor(u"!Ref", construct_function_ignore)
-yaml.add_constructor(u"!GetAtt", construct_function_ignore)
-yaml.add_constructor(u"!Sub", construct_function_ignore)
-yaml.add_constructor(u"!Join", construct_function_ignore)
-yaml.add_constructor(u"!If", construct_function_ignore)
-yaml.add_constructor(u"!Not", construct_function_ignore)
-yaml.add_constructor(u"!Equals", construct_function_ignore)
-yaml.add_constructor(u"!Select", construct_function_ignore)
-yaml.add_constructor(u"!GetAZs", construct_function_ignore)
-yaml.add_constructor(u"!Or", construct_function_ignore)
 
 if __name__ == "__main__":
     stacker.main()
